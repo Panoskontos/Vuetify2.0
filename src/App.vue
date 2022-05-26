b4c522d2ccfefdab14dbe338d4245d3bc6f60668
Panoskontos <panos96el@gmail.com>
2022-05-26 17:06:23 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,187 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <v-app>
+    <div class="body">
+    <!-- Toolbar -->
+     <v-app-bar class="flex-grow-0 appbar" app dark>
+    
+    
+    <v-app-bar-title></v-app-bar-title>
+
+      <v-spacer></v-spacer>
+        
+              <v-btn icon>
+                <v-icon>mdi-magnify</v-icon>
+              </v-btn>
+
+      <v-btn icon>
+        <v-badge
+        color="red"
+          dot
+          overlap
+                  >
+
+              <v-icon>mdi-bell</v-icon>
+          </v-badge>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+
+
+   </v-app-bar>
+
+    <!-- Navbar -->
+      <v-navigation-drawer app v-model="drawer"
+      expand-on-hover
+      dark
+      permanent
+      width="250px"
+      height="100vh"
+      class="rounded-md blue-grey darken-4 sidenav"
+      >
+<!-- empty item -->
+      <!-- <v-list-item class="mt-2">
+        <v-list-item-content>
+          <v-list-item-title class="text-h6"> 
+            
+            </v-list-item-title>
+        </v-list-item-content>
+</v-list-item> -->
+
+<!--  -->
+        <!-- <v-divider></v-divider> -->
+        <v-list>
+          <v-list-item class="px-2 pt-4  d-flex justify-center">
+            <v-list-item-avatar class="grey darken-4 ">
+              <v-img src="https://lh3.googleusercontent.com/-4_ZEHFWjQ10/AAAAAAAAAAI/AAAAAAAAAAA/C8YPLHf4HqQ/s55-p-k-no-ns-nd/photo.jpg"></v-img>
+            </v-list-item-avatar>
+          </v-list-item>
+
+<br>
+            <v-divider></v-divider>
+
+
+          <v-list-item class="px-2 pt-1  d-flex justify-space-around" link>
+            <v-list-item-avatar>
+              <v-img src="https://randomuser.me/api/portraits/men/90.jpg"></v-img>
+            </v-list-item-avatar>
+            <v-list-item-content>
+
+              <v-list-item-title class="text-h8">
+                Panos Kontos
+              </v-list-item-title>
+<v-list-item-subtitle>panos@gmail.com</v-list-item-subtitle>
+            </v-list-item-content>
+
+          </v-list-item>
+
+            <v-divider></v-divider>
+
+          
+      
+
+          <!-- <v-list-item link>
+            <v-list-item-content>
+              <v-list-item-title class="text-h8">
+                Panos Kontos
+              </v-list-item-title>
+              <v-list-item-subtitle>panos@gmail.com</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item> -->
+        </v-list>
+
+        <!-- <v-divider></v-divider> -->
+
+        <v-list
+          nav
+          dense
+        >
+
+<!-- Icons https://icon-sets.iconify.design/mdi/ -->
+
+ <router-link to="/">
+        <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+  </router-link >
+
+<router-link to="/about">
+      <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-vector-point</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Minions</v-list-item-title>
+          </v-list-item>
+</router-link>
+          
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-key-chain</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Keys</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-play</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Jobs</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-account-multiple</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>About</v-list-item-title>
+          </v-list-item>
+      
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main >
+        
+          <router-view/>
+        </v-main>
+     
+       
+     
+
+
   </div>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => {
+      return {
+         drawer: true,
+        }
+     }
+};
+</script>
+
+<style scoped>
+a {
+    text-decoration: none !important;
 }
 
-nav {
-  padding: 30px;
+
+#app{
+  background-color: #1e1e24;
 }
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+
+.appbar {
+  background-color: #1e1e24 !important;
+
 }
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,112 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div>
+ <div>
+    <div height="225px" class="row transparent mt-5 mx-10 d-flex justify-center" >
+
+      
+      <v-card 
+       v-bind="props"
+        :class="`elevation-${isHovering ? 24 : 6}`"
+      height="225px" width="60%" class="row-card blue-grey darken-3 mr-10">
+
+      <ConformityComponent/>
+
+      </v-card>      
+       
+      
+       
+      <v-card 
+       v-bind="props"
+        :class="`elevation-${isHovering ? 24 : 6}`"
+      height="225px" width="20%" class="row-card row-card-keys blue-grey darken-3 ">
+
+      <KeysComponent/>
+
+</v-card>
+        
+      
+      
+       
+      <v-card 
+       v-bind="props"
+        :class="`elevation-${isHovering ? 24 : 6}`"
+      height="225px" width="20%" class="row-card row-card-status blue-grey darken-3 ml-10">
+
+  <StatusComponent/>
+
+</v-card>
+              
+
+    </div>
+    
+    <div  height="200px" class="bg-transparent mx-10 mt-10 d-flex justify-center" > 
+        
+         
+      <v-card 
+       v-bind="props"
+        :class="`elevation-${isHovering ? 24 : 6}`"
+        
+        height="230px" width="50%" class="blue-grey darken-3 mr-10">
+        </v-card>      
+          
+
+     
+      <v-card 
+       v-bind="props"
+        :class="`elevation-${isHovering ? 24 : 6}`"
+         height="230px" width="50%" class="blue-grey darken-3 ">
+        </v-card>
+
+           
+    
+    </div>
+    
+      
+    <v-card 
+    v-bind="props"
+        :class="`elevation-${isHovering ? 24 : 6}`"
+    height="320px" class="blue-grey darken-3 mx-10 mt-10" 
+     :style="{ 'background-color': hover ? 'red' : '#BB86FC' }"
+    >      
+    </v-card>
+      
+
+    </div>
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  export default {
+    name: 'HomeView',
+
+    components: {
+    },
   }
-}
 </script>
+
+<style>
+ @media only screen and (max-width: 1000px) { 
+  .row {
+    background-color: rgb(255, 255, 255);
+    width: 80%;
+    height: 750px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .row-card {
+    width: 100% !important;
+    margin-bottom: 20px;
+    height: 205px !important;
+    margin: auto !important;
+  }
+  .row-card-keys {
+    /* height: 225px !important; */
+  }
+  .row-card-status {
+    margin-right: 37px !important;
+    /* height: 225px !important; */
+  }
+} 
+</style>
